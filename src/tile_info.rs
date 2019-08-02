6f0a3d1963acf0fdc24ea99bd494c15de56142cf
Ophir LOJKINE <ophir.lojkine@auto-grid.com>
2019-08-02 12:07:16 +0200

    Implement page source parsing

--- src/tile_info.rs
+++ src/tile_info.rs
@@ -2,6 +2,8 @@ use std::str::FromStr;
 
 use serde::Deserialize;
 
+use custom_error::custom_error;
+
 #[derive(Debug, Deserialize, PartialEq)]
 pub struct TileInfo {
     tile_width: u32,
@@ -30,8 +32,51 @@ impl FromStr for TileInfo {
     }
 }
 
+pub struct PageInfo {
+    path: String,
+    token: String,
+}
+
+impl FromStr for PageInfo {
+    type Err = PageParseError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let path = extract_between(
+            s,
+            "<meta property=\"og:image\" content=\"",
+            "\"",
+        ).ok_or(PageParseError::NoPath)?.to_string();
+
+        let path_no_protocol = path.split(':')
+            .skip(1)
+            .next().ok_or(PageParseError::BadPath)?;
+        let before_token = format!(",\"{}\",\"", path_no_protocol);
+        let token = extract_between(s, &before_token, "\"")
+            .ok_or(PageParseError::NoToken)?.to_string();
+        Ok(PageInfo { path, token })
+    }
+}
+
+fn extract_between<'a, 'b, 'c>(s: &'a str, start: &'b str, end: &'c str)
+                               -> Option<&'a str> {
+    let start_pos = start.len() + s.find(start)?;
+    let end_pos = start_pos + (&s[start_pos..]).find(end)?;
+    Some(&s[start_pos..end_pos])
+}
+
+custom_error! {pub PageParseError
+    NoPath  = "Unable to find path information",
+    BadPath = "The path has an invalid form",
+    NoToken = "Unable to find the token in the page",
+}
+
 #[cfg(test)]
 mod tests {
+    use std::fs;
+    use std::path::Path;
+
+    use crate::tile_info::{extract_between, PageInfo};
+
     use super::TileInfo;
 
     #[test]
@@ -49,5 +94,21 @@ mod tests {
         dbg!(&infos);
         assert_eq!(infos.tile_width, 512);
         assert_eq!(infos.pyramid_level[4].num_tiles_x, 11);
+    }
+
+    #[test]
+    fn test_extract_between() {
+        assert_eq!(extract_between("A B C", "A ", " C"), Some("B"));
+    }
+
+    #[test]
+    fn test_parse_html() {
+        let test_source_path = Path::new(env!("CARGO_MANIFEST_DIR"))
+            .join("testdata")
+            .join("page_source.html");
+        let test_html = fs::read_to_string(test_source_path).unwrap();
+        let info: PageInfo = test_html.parse().unwrap();
+        assert_eq!(info.path, "https://lh5.ggpht.com/4AX4ua174encReZyEE7dTu0_RgBrBi79iqHamKQJtZnIBA5xqKBQib8DNvnq");
+        assert_eq!(info.token, "RQhR1krE-uvCYNXm5CmP6k2MuPY");
     }
 }
